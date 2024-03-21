1fb3fa99c1db9bd3c0dad070acd508ca183309af
Jadir Xavier <49170436+JadirXavier@users.noreply.github.com>
2024-03-21 21:52:43 +0000

    Changed some models and started create listing layout

--- auctions/models.py
+++ auctions/models.py
@@ -3,29 +3,47 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return f"{self.username} {self.email} {self.password}"
+
 
 class Category(models.Model):
     title = models.CharField(max_length = 20)
 
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name_plural = "categories"
+
 class Auction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_auctions")
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="category_auctions")
+    categories = models.ManyToManyField(Category, related_name="categories_auctions")
     title = models.CharField(max_length = 50)
     description = models.TextField(blank=True)
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
     current_bid = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='images/')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} {self.user} {self.starting_bid} {self.created_at}"
+
     
 class Bid(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="user_bids")
-    auction = models.ForeignKey(Auction, on_delete=models.SET_NULL, related_name="auction_bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bids")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="auction_bids")
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.auction} {self.user} {self.amount} {self.created_at}"
+
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="user_comments")
-    auction = models.ForeignKey(Auction, on_delete=models.SET_NULL, related_name="auction_comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="auction_comments")
     text = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} {self.auction} {self.text} {self.created_at}"
