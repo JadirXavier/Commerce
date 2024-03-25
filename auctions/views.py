faf72873eb2c09c8255181d0831de10b27d808aa
jadir1 <joxf1994@gmail.com>
2024-03-25 19:35:19 -0300

    Createned new categories in database and updated new_listing funcion and model to get categories from database

--- auctions/views.py
+++ auctions/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Category
 
 
 def index(request):
@@ -63,4 +63,8 @@ def register(request):
         return render(request, "auctions/register.html")
 
 def new_listing(request):
-    return render(request, "auctions/new_listing.html")
+    if request.method == "GET":
+        categories = Category.objects.all()
+        return render(request, "auctions/new_listing.html",{
+            "categories": categories
+        })
